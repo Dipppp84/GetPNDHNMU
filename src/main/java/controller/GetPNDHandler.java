ed098d8913df8e7dc9a9e29e29d908fdc155cf82
Dipppp84 <dipppp84@gmail.com>
2021-12-30 17:09:23 +0200

    Работает

---
+++ src/main/java/controller/GetPNDHandler.java
@@ -0,0 +1,55 @@
+package controller;
+
+import DAO.DAO_PND;
+import entity.PND;
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.router.RouterNanoHTTPD;
+import utill.GsonUtil;
+import utill.ServerHttp;
+
+import java.util.List;
+import java.util.Map;
+import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
+
+public class GetPNDHandler extends RouterNanoHTTPD.DefaultHandler {
+    @Override
+    public NanoHTTPD.Response get(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
+        List<PND> pndArrayList = DAO_PND.getDAO().getPND();
+        return ServerHttp.getCors(newFixedLengthResponse(GsonUtil.getToGson().getGson(pndArrayList)));
+    }
+
+    @Override
+    public NanoHTTPD.Response post(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
+        return newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "");
+    }
+
+    @Override
+    public NanoHTTPD.Response put(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
+        return newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "");
+    }
+
+    @Override
+    public NanoHTTPD.Response delete(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
+        return newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "");
+    }
+
+    @Override
+    public NanoHTTPD.Response other(String method, RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, NanoHTTPD.IHTTPSession session) {
+        return newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, NanoHTTPD.MIME_PLAINTEXT, "");
+    }
+
+    @Override
+    public String getText() {
+        return null;
+    }
+
+    @Override
+    public String getMimeType() {
+        return null;
+    }
+
+    @Override
+    public NanoHTTPD.Response.IStatus getStatus() {
+        return NanoHTTPD.Response.Status.OK;
+    }
+}
--- src/main/java/utill/ServerHttp.java
+++ src/main/java/utill/ServerHttp.java
@@ -1,11 +1,10 @@
 package utill;
 
 import controller.DelPNDHandler;
+import controller.GetPNDHandler;
 import controller.PNDHandler;
-import entity.PND;
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.router.RouterNanoHTTPD;
-import lombok.SneakyThrows;
 
 import java.io.IOException;
 
@@ -22,6 +21,7 @@ public class ServerHttp extends RouterNanoHTTPD {
         setNotImplementedHandler(RouterNanoHTTPD.NotImplementedHandler.class);
         setNotFoundHandler(RouterNanoHTTPD.Error404UriHandler.class);
         addRoute("/PND", PNDHandler.class);
+        addRoute("/getPND", GetPNDHandler.class);
         addRoute("/delPND", DelPNDHandler.class);
     }
 
